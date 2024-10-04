13ea12a506828ba7febd4c7156a63d321753d092
mgi388 <135186256+mgi388@users.noreply.github.com>
2024-10-04 20:27:00 +1000

    add JSON format flag to army edit CLI

--- src/cli/army/mod.rs
+++ src/cli/army/mod.rs
@@ -4,7 +4,7 @@ use std::{
     path::PathBuf,
 };
 
-use clap::{Args, Subcommand};
+use clap::{Args, Subcommand, ValueEnum};
 use darkomen::army::*;
 
 #[derive(Debug, Args)]
@@ -27,62 +27,89 @@ pub struct EditArmyArgs {
     /// The name of the text editor to use.
     #[arg(short, long, default_value = "code --wait")]
     pub editor: String,
+
+    /// The format to edit the army file in.
+    #[arg(short, long, default_value_t=Format::Json)]
+    #[clap(value_enum)]
+    pub format: Format,
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+pub enum Format {
+    Json,
+    Ron,
 }
 
 pub fn run(args: &ArmyArgs) -> anyhow::Result<()> {
     if let Some(ArmySubcommands::Edit(edit_args)) = &args.subcommand {
-        edit_army_file(edit_args.editor.clone(), edit_args.army_file.clone().into())?;
+        edit_army_file(edit_args)?;
     }
 
     Ok(())
 }
 
-fn edit_army_file(editor: String, file_path: PathBuf) -> anyhow::Result<()> {
+fn edit_army_file(args: &EditArmyArgs) -> anyhow::Result<()> {
+    let army_file: PathBuf = args.army_file.clone().into();
+
     // Load the army file.
-    let file = File::open(file_path.clone())?;
+    let file = File::open(army_file.clone())?;
     let army = Decoder::new(file).decode()?;
 
-    // Serialize the army to RON.
-    let ron_string = ron::ser::to_string_pretty(&army, ron::ser::PrettyConfig::default())?;
+    // Serialize the army to a human-readable string.
+    let (as_string, extension) = match args.format {
+        Format::Ron => (
+            ron::ser::to_string_pretty(&army, ron::ser::PrettyConfig::default())?,
+            "ron",
+        ),
+        Format::Json => (serde_json::to_string_pretty(&army)?, "json"),
+    };
 
-    // Write the RON string to a temporary file.
-    let prefix = file_path
-        .file_stem()
-        .and_then(|stem| stem.to_str())
-        .unwrap_or("army")
-        .to_owned()
-        + ".";
+    // Write the human-readable string to a temporary file.
+    let prefix = format!(
+        "{}.",
+        army_file
+            .file_stem()
+            .and_then(|stem| stem.to_str())
+            .unwrap_or("army"),
+    );
+    let suffix = format!(".{}", extension);
     let mut temp_file = tempfile::Builder::new()
         .prefix(&prefix)
-        .suffix(".ron")
+        .suffix(&suffix)
         .tempfile()?;
-    temp_file.write_all(ron_string.as_bytes())?;
+    temp_file.write_all(as_string.as_bytes())?;
     temp_file.flush()?;
 
     // Open the temporary file in the editor.
     let (editor, editor_args) = {
-        let mut parts = editor.split_whitespace();
+        let mut parts = args.editor.split_whitespace();
         let editor = parts.next().unwrap();
         let editor_args = parts.collect::<Vec<_>>();
         (editor, editor_args)
     };
     let mut command = std::process::Command::new(editor);
     command.args(editor_args);
+
     // This call blocks until the editor process exits.
+    println!("Waiting for editor to close...");
     command.arg(temp_file.path()).status()?;
+    println!("Editor closed");
 
-    // Read the modified RON string from the temporary file.
-    let mut modified_ron_string = String::new();
-    temp_file
-        .reopen()?
-        .read_to_string(&mut modified_ron_string)?;
+    // Read the modified human-readable string from the temporary file.
+    let mut modified_string = String::new();
+    temp_file.reopen()?.read_to_string(&mut modified_string)?;
 
-    // Deserialize the modified RON string to an army.
-    let modified_army = ron::de::from_str(&modified_ron_string)?;
+    // Deserialize the modified string to an army.
+    let modified_army = match args.format {
+        Format::Ron => ron::de::from_str(&modified_string)?,
+        Format::Json => serde_json::from_str(&modified_string)?,
+    };
 
     // Write the modified army to the original file.
-    let file = File::create(file_path)?;
+    let file = File::create(army_file)?;
     Encoder::new(file).encode(&modified_army)?;
+
+    println!("Army file successfully edited");
 
     Ok(())
 }
