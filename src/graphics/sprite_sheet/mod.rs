6a18e33ddfa7f68f7c4c447f5d5c3c32346103fa
mgi388 <135186256+mgi388@users.noreply.github.com>
2024-11-07 20:23:24 +1100

    add TextureDescriptor.anchor for sprite sheet (#164)

--- src/graphics/sprite_sheet/mod.rs
+++ src/graphics/sprite_sheet/mod.rs
@@ -4,6 +4,7 @@ mod zeroruns;
 
 #[cfg(feature = "bevy_reflect")]
 use bevy_reflect::prelude::*;
+use glam::Vec2;
 use image::DynamicImage;
 use serde::Serialize;
 
@@ -27,4 +28,13 @@ pub struct TextureDescriptor {
     pub y: i16,
     pub width: u16,
     pub height: u16,
+}
+
+impl TextureDescriptor {
+    pub fn anchor(&self) -> Vec2 {
+        Vec2::new(
+            (self.x.abs() as f32 - (self.width as f32 / 2.0)) / self.width as f32,
+            (self.y.abs() as f32 - (self.height as f32 / 2.0)) / self.height as f32,
+        )
+    }
 }
