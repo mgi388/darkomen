8fa6474a9abed3b7f6fc3394b697ff7ae4b2c0c4
mgi388 <135186256+mgi388@users.noreply.github.com>
2024-11-08 12:05:56 +1100

    add music script asset plugin

--- src/asset/sound/mod.rs
+++ src/asset/sound/mod.rs
@@ -1,8 +1,11 @@
 use bevy_app::prelude::*;
 
-use crate::asset::sound::{mad::MonoAudioAssetPlugin, sad::StereoAudioAssetPlugin};
+use crate::asset::sound::{
+    mad::MonoAudioAssetPlugin, music_script::MusicScriptAssetPlugin, sad::StereoAudioAssetPlugin,
+};
 
 pub mod mad;
+pub mod music_script;
 pub mod sad;
 
 pub struct SoundAssetPlugin;
@@ -14,6 +17,9 @@ impl Plugin for SoundAssetPlugin {
         }
         if !app.is_plugin_added::<StereoAudioAssetPlugin>() {
             app.add_plugins(StereoAudioAssetPlugin);
+        }
+        if !app.is_plugin_added::<MusicScriptAssetPlugin>() {
+            app.add_plugins(MusicScriptAssetPlugin);
         }
     }
 }
