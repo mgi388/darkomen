2d26896406b547c4d7cfacc6a5181b40d96662bc
mgi388 <135186256+mgi388@users.noreply.github.com>
2025-02-24 21:00:50 +1100

    add different sound features (#271)

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,14 @@ pub mod m3d;
 pub mod project;
 pub mod shadow;
 pub mod sound;
+
+pub mod prelude {
+    #[doc(hidden)]
+    pub use crate::army::{Army, ArmyRace};
+    #[doc(hidden)]
+    pub use crate::battle_tabletop::BattleTabletop;
+    #[doc(hidden)]
+    pub use crate::project::{Heightmap, Instance, Project};
+    #[doc(hidden)]
+    pub use crate::sound::sfx::{Packet, Sfx, SfxFlags, SfxId, SfxType, Sound};
+}
